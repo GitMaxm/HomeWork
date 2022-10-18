5538f07d12279e712dc47b9770b99ae8f42aa70f
Ivan <ivanenkomax@rambler.ru>
2022-10-18 18:33:06 +0300

    Выходной день

---
+++ Семинар 2/Task_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Ведите номер дня недели: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number == 1) Console.Write("Понедельник ");
+if (number == 2) Console.Write("Вторник ");
+if (number == 3) Console.Write("Среда ");
+if (number == 4) Console.Write("Четверг ");
+if (number == 5) Console.Write("Пятница ");
+if (number == 6) Console.Write("Суббота ");
+if (number == 7) Console.Write("Воскресенье ");
+
+
+if (number < 6) 
+{ 
+    Console.WriteLine("будний день");
+}
+else
+{
+    Console.WriteLine("выходной день");
+}
