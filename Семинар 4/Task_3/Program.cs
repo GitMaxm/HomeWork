0440c785136519023a143f53d6ab7e77f43e83fd
Ivan <ivanenkomax@rambler.ru>
2022-10-23 16:48:28 +0300

    массив из 8 элементов

---
+++ Семинар 4/Task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+int[] creatArray()
+{
+    int[] ints = new int[8];
+    Random random = new Random();
+    for (int i = 0; i < ints.Length; i++)
+    {
+        ints[i] = random.Next();
+    }
+    return ints;
+}
+
+string GetArrayAsString(int[] array)
+{
+    string result = "";
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += $"{array[i].ToString()}, ";
+    }
+    return result;
+}
+
+Console.WriteLine(GetArrayAsString(creatArray()));
