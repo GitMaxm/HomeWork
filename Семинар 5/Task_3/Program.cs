a6a7c5216acb85b81623f718df0f4c675b966628
Ivan <ivanenkomax@rambler.ru>
2022-10-24 17:26:35 +0300

    +

--- Семинар 5/Task_3/Program.cs
+++ Семинар 5/Task_3/Program.cs
@@ -1,2 +1,23 @@
 ﻿//Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 //[3 7 22 2 78] -> 76
+
+int[] CreateArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue);
+    }
+    return array;
+}
+
+int GetFindMaxMin(int[] array);
+{
+    int max = 0;
+    for (int j = 0; j < array.Length; j++)
+    {
+        if(array[i] > arra)
+    }
+
+}
