bca8f354b5ccd2f74d54fc8fc1e70c2ba8a0b3a2
Ivan <ivanenkomax@rambler.ru>
2022-10-27 13:18:10 +0300

    +

--- Семинар 7/Task_1/Program.cs
+++ Семинар 7/Task_1/Program.cs
@@ -4,11 +4,11 @@ Console.WriteLine("enter m: ");
 int m = int.Parse(Console.ReadLine());
 Console.WriteLine("enter n: ");
 int n = int.Parse(Console.ReadLine());
-
+double[,] array = new double[m, n];
 
 void GetArray(double[,] array)
 {
-double[,] array = new double[m, n];
+
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
