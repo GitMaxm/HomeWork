667301469fd026b3ef47c55badd8593658048f37
Ivan <ivanenkomax@rambler.ru>
2022-10-27 15:44:21 +0300

    +

---
+++ Семинар 6/Task_1/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите колличество элементов массива: ");
+int size = int.Parse(Console.ReadLine());
+int[] myArray = new int[size];
+int sum = 0;
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write($"Введите элемент масива под индексом {i}: ");
+    myArray[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write($"{myArray[i]} ");
+}
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i] > 0)
+    {
+        sum++;
+    }
+}
+Console.ReadLine();
+Console.WriteLine($"Чисел больше нуля: {sum}");
