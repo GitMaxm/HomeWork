af11a0b56171515e47d57119af34913b5feed8d5
Ivan <ivanenkomax@rambler.ru>
2022-10-28 19:23:35 +0300

    +

---
+++ Семинар 7/Task_2/Program.cs
@@ -0,0 +1,52 @@
+﻿//  Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+
+
+
+Console.WriteLine("enter m: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("enter n: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] array = new int[m, n];
+int[,] GetArray(int m, int n)
+
+{
+
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(1, 50);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+if (n > array.GetLength(0) || m > array.GetLength(1))
+{
+    Console.WriteLine("Такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"Значение элемента {n} строки и {m} столбца равно {array[n - 1, m - 1]}");
+}
+
+PrintArray(GetArray(m, n));
