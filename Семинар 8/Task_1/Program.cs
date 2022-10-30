763a0a135e7ed87b331e0776076ae14511750510
Ivan <ivanenkomax@rambler.ru>
2022-10-30 23:59:00 +0300

    +

---
+++ Семинар 8/Task_1/Program.cs
@@ -0,0 +1,55 @@
+﻿//  Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+Console.Write("Номер строки: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Номер стобца: ");
+int n = int.Parse(Console.ReadLine());
+int[,] array = new int[m, n];
+
+
+int[,] GetArray(int m, int n)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+array = GetArray(m,n);
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1) - 1; j++)
+    {
+        for (int z = 0; z < array.GetLength(1) - 1; z++)
+        {
+            if (array[i, z] > array[i, z + 1]) 
+            {
+                int temp = 0;
+                temp = array[i, z];
+                array[i, z] = array[i, z + 1];
+                array[i, z + 1] = temp;
+            }
+        }
+    }
+}
+
+PrintArray(array);
