5ddbae45c69800552d9572e1113138d9e921bc1d
Ivan <ivanenkomax@rambler.ru>
2022-11-01 16:29:39 +0300

    +

---
+++ Семинар 9/Task_2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+Console.Write("Введите M: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Введите N: ");
+int N = int.Parse(Console.ReadLine());
+int sum = 0;
+
+void GetNumbersMN(int M, int N)
+{
+    if (N >= M)
+    {
+        if (N == M)
+        {
+            Console.WriteLine(N);
+            return;
+        }
+        else
+        {
+            GetNumbersMN(M, N - 1);
+            Console.WriteLine(N);
+        }
+    }
+    else Console.WriteLine($"{M} > {N} введите правильные данные");
+}
+GetNumbersMN(M, N);
+
+void PrintSum(int M, int N, int sum)
+{
+    sum = sum + M;
+    if (M <= N)
+    {
+        Console.Write($"Сумма натуральных элементов: {sum}");
+        return;
+    }
+    PrintSum(M, N - 1, sum);
+}
+
+PrintSum(M, N - 1, sum);
